3a1375f4a26dd294f3b94bbf026cccd8bc0e4887
vasanth0059 <vasanthbabukassa@gmail.com>
2024-07-29 11:48:27 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,10 @@
-# # # def triangle(n):
-# # #     for i in range(n):
-# # #         print("1"*n)
-# # # triangle(3)
-# n=int(input())
-# # print('1'*n)
-# # for i in range(n):
-# #     for j in range(n):
-# #         print('*'*n)
+def triangle(n):
+    for i in range(n):
+        print("1"*n)
+triangle(3)
+n=int(input())
+print('1'*n)
+for i in range(n):
+    for j in range(n):
+        print('*'*n)
 
